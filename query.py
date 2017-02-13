7658035e35b402d40c9151a3e4662ff1abe51c53
Christan <christanskordas@gmail.com>
2017-02-13 19:48:09 +0200

    new_changes

--- query.py
+++ query.py
@@ -8,11 +8,11 @@ collection = db.FirstCollection
 class Query:
 	""" This class represents a user sub-query """
 
-	def __init__(self, qid, duration=0, realx1=None, realy1=None, realx2=None, realy2=None):
+	def __init__(self, qid, duration=1, realx1=None, realy1=None, realx2=None, realy2=None, episodes = 'MOVE'):
 		""" Constructor: Initialize the sub-query and has as members the columns that will be used """
 		self.qid = qid
 		self.QueryCount = -1
-		self.episodes = []
+		self.episodes = episodes
 		self.duration = duration
 		self.realx1 = realx1
 		self.realy1 = realy1
@@ -24,7 +24,6 @@ class Query:
 		self.realy1 = self.realy1 + steps_ * distortion.area_step
 		self.realx2 = self.realx2 + steps_ * distortion.area_step
 		self.realy2 = self.realy2 + steps_ * distortion.area_step
-		pass
 
 	def get_results(self):
 		"""Returns the trajectories, as tr_ids, that included in the parameters, the user set at the sub-query"""
@@ -33,14 +32,16 @@ class Query:
 		if self.realx1 is None or self.realx2 is None or self.realy1 is None or self.realy2 is None:
 			return None
 
-		documents = db.FirstCollection.find({ "loc": { "$geoWithin": { "$box": [ [self.realx1, self.realy1],
-                                                                             [self.realx2, self.realy2] ] } } } )
+		documents = db.FirstCollection.find({"loc": {"$geoWithin": {"$box": [
+			[self.realx1, self.realy1], [self.realx2, self.realy2]]}}, "duration": {"$lt": self.duration},
+			"episodes": self.episodes })
 
-		trajectories_ids = []
+		trajc_ids = []
 
 		for doc in documents:
-			trajectories_ids.append(doc['MOid'])
-
+			trajc_ids.append(doc['MOid'])
+		traj_id = set(trajc_ids)
+		trajectories_ids = list(traj_id)
 		return trajectories_ids
 
 	@staticmethod
@@ -68,6 +69,7 @@ class Query:
 
 		results = total_results[0]
 		
-		for r in range(len(results)):
-			results = set(results).intersection(results[r])
-		return results
+		for r in range(len(total_results)):
+			results = set(results).intersection(total_results[r])
+		result = list(results)
+		return result
