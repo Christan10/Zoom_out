2e738681bc4f7bc138562e3bd98bac8a618220da
Christan <christanskordas@gmail.com>
2017-02-13 19:47:02 +0200

    new_changes

--- zoom_out.py
+++ zoom_out.py
@@ -1,5 +1,5 @@
 import query
-import distortion
+from distortion import *
 import constants
 from pymongo import MongoClient
 client = MongoClient()
@@ -28,27 +28,27 @@ class HRow:
                 return True
         return False
 
-    def set_subquery_dist_area_unit(self, sbqid, area_dist_unit):
+    def set_subquery_dist_area_unit(self, sbqid, area_dist_unit, steps):
         if self.subquery_exists(sbqid) is True:
             raise Exception("Attempt to add area distortion for subquery that does not need distortion")
-        self.SQ_dis_units.append({'type': 'area_dist', 'id': sbqid, 'value': area_dist_unit})
+        self.SQ_dis_units.append({'type': 'area_dist', 'id': sbqid, 'value': area_dist_unit, 'steps': steps})
 
-    def set_subquery_dist_time_unit(self, sbqid, time_dist_unit):
+    def set_subquery_dist_time_unit(self, sbqid, time_dist_unit, steps):
         if self.subquery_exists(sbqid) is True:
             raise Exception("Attempt to add time distortion for subquery that does not need distortion")
-        self.SQ_dis_units.append({'type': 'time_dist', 'id': sbqid, 'value': time_dist_unit})
+        self.SQ_dis_units.append({'type': 'time_dist', 'id': sbqid, 'value': time_dist_unit, 'steps': steps})
 
-    def set_subquery_dist_area_time_unit(self, sbqid, area_time_dist_unit):
+    def set_subquery_dist_area_time_unit(self, sbqid, area_time_dist_unit, steps):
         if self.subquery_exists(sbqid) is True:
             raise Exception("Attempt to add area-time distortion for subquery that does not need distortion")
-        self.SQ_dis_units.append({'type': 'area_time_dist', 'id': sbqid, 'value': area_time_dist_unit})
+        self.SQ_dis_units.append({'type': 'area_time_dist', 'id': sbqid, 'value': area_time_dist_unit, 'steps': steps})
 
     def get_min_area_dist_unit(self):
         """This function finds the sub-query id (sbqid) that has the minimum distortion unit value (only for area
         distortion) from a row's SQ_dis_units, and returns it with the value."""
         min_dist = constants.MIN_DIST
         sbqid = constants.INVALID_ID
-
+        step = constants.INVALID_ID
         for e in range(len(self.SQ_dis_units)):
             map = self.SQ_dis_units[e]
             if map['type'] == 'area_dist':
@@ -57,15 +57,16 @@ class HRow:
                 if dist_val < min_dist:
                     min_dist = dist_val
                     sbqid = map['id']
+                    step = map['steps']
 
-        return (sbqid, min_dist)
+        return (sbqid, min_dist, step)
 
     def get_min_time_dist_unit(self):
         """This function finds the sub-query id (sbqid) that has the minimum distortion unit value (only for time
         distortion) from a row's SQ_dis_units, and returns it with the value."""
         min_dist = constants.MIN_DIST
         sbqid = constants.INVALID_ID
-
+        step = constants.INVALID_ID
         for e in range(len(self.SQ_dis_units)):
             map = self.SQ_dis_units[e]
             if map['type'] == 'time_dist':
@@ -74,8 +75,9 @@ class HRow:
                 if dist_val < min_dist:
                     min_dist = dist_val
                     sbqid = map['id']
+                    step = map['steps']
 
-        return (sbqid, min_dist)
+        return (sbqid, min_dist, step)
 
     def get_min_area_time_dist_unit(self):
         """This function finds the sub-query id (sbqid) that has the minimum distortion unit value (for area and time
@@ -135,7 +137,6 @@ class HMat:
         for r in range(len(self.Hmat)):
             row = self.Hmat[r]
             row_freq = row.freq
-
             if row_freq < k:
                 if row_freq > next_max_freq:
                     next_max_freq = row_freq
@@ -173,9 +174,9 @@ def zoom(K, Q, Rmin, Rmax, distortion):
     F_Q = Q
     hmat = HMat()
     ntr = len(query.Query.combine(F_Q))
-
-    if ntr == K:
-        return Q
+    print(ntr)
+    if ntr >= K or ntr == 0:
+        raise Exception("algorithm will not run")
 
     sth_changed = True
     k = len(F_Q)
@@ -183,10 +184,11 @@ def zoom(K, Q, Rmin, Rmax, distortion):
     while ntr != K and sth_changed is True:
         sth_changed = False
         for i in range(len(F_Q)):
-            rslt = F_Q[i].get_results()
+            f_q = F_Q[i]
+            rslt = f_q.get_results()
             if rslt is not None:
                 for r in rslt:
-                    hmat.fill(rslt[r], F_Q[i])
+                    hmat.fill(r, f_q)
 
         freq = hmat.get_init_max_freq(k)
 
@@ -205,14 +207,15 @@ def zoom(K, Q, Rmin, Rmax, distortion):
 
             row_counter += len(next_max_freq_rows)
             episode_found = distortion.compute_rows_distortions(next_max_freq_rows)
+
             if episode_found is True:
                 sth_changed = True
-                ntr = Query.combine(F_Q)
+                ntr = len(query.Query.combine(F_Q))
+
             if episode_found is False:
                 freq = hmat.get_next_max_freq(k, freq)
                 next_max_freq_rows = hmat.find_next_max_freq_rows(k, freq)
-        #
-        #
+
     return F_Q
 
 
