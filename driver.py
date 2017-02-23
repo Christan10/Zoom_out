5db8da7a7ccacc7480634ccec3924f56856227a4
Christan <christanskordas@gmail.com>
2017-02-24 01:54:15 +0200

    changes

--- driver.py
+++ driver.py
@@ -2,18 +2,18 @@ from query import *
 from zoom_out import *
 from distortion import *
 
-K = 22
+K = 87
 Rmax = 1.0
 Rmin = 0.5
 
 Q = []
 
-Q_1 = Query(1, 500000, 472518.0, 4201118.0, 482000.0, 4209000.0)
+Q_1 = Query(1, 500000, 472518.0, 4201118.0, 482000.0, 4206000.0)
 Q_1.episodes = 'CAR'
 Q.append(Q_1)
 
-Q_2 = Query(2, 500000, 473518.0, 4201118.0, 480000.0, 4209000.0)
-Q_2.episodes = 'BUS'
+Q_2 = Query(2, 500000, 473318.0, 4201118.0, 480000.0, 4209000.0)
+Q_2.episodes = 'CAR'
 Q.append(Q_2)
 
 #Q_3 = Query(3, 0, 460000, 4100000, 480000, 4300000)
@@ -22,7 +22,7 @@ Q.append(Q_2)
 
 #declare the object that handles the distortions
 
-distortionData = DistortionData(20000.0, 0, True)
+distortionData = DistortionData(2500.0, 0, True)
 
 #set queries for distortion data
 distortionData.set_subqueries(Q)
@@ -30,6 +30,6 @@ distortionData.set_subqueries(Q)
 
 FQ = zoom(K, Q, Rmin, Rmax, distortionData)
 
-print(Q)
+print(Q_1, Q_2.realx1, Q_2.realx2, Q_1.realy2)
 
 
