324745c31d052115dc0e26d38470639b9f736cb5
Christan <christanskordas@gmail.com>
2017-03-09 21:09:50 +0200

    final

--- driver.py
+++ driver.py
@@ -3,23 +3,23 @@ from zoom_out import *
 from distortion import *
 
 
-K = 87
-Rmax = 1.0
-Rmin = 0.5
+K = 50
+Rmax = 1.7
+Rmin = 1.0
 
 Q = []
 
-Q_1 = Query(1, 500000, 472518.0, 4201118.0, 482000.0, 4206000.0)
+Q_1 = Query(1, 500000, 473318.0, 4201118.0, 480000.0, 4209000.0)
 Q_1.episodes = 'CAR'
 Q.append(Q_1)
 
-Q_2 = Query(2, 500000, 473318.0, 4201118.0, 480000.0, 4209000.0)
+Q_2 = Query(2, 500000, 472518.0, 4201118.0, 482000.0, 4206000.0)
 Q_2.episodes = 'CAR'
 Q.append(Q_2)
 
-#Q_3 = Query(3, 0, 460000, 4100000, 480000, 4300000)
-#Q_3.episodes = ['HOME']
-#Q.append(Q_3)
+Q_3 = Query(3, 500000, 460000.0, 4200000.0, 470000.0, 4202000.0)
+Q_3.episodes = 'MOVE'
+Q.append(Q_3)
 
 #declare the object that handles the distortions
 
@@ -31,6 +31,7 @@ distortionData.set_subqueries(Q)
 
 FQ = zoom(K, Q, Rmin, Rmax, distortionData)
 
-print(Q_1, Q_2.realx1, Q_2.realx2, Q_1.realy2)
+print(Q_1.realx1, Q_1.realy1, Q_1.realx2, Q_1.realy2, Q_2.realx1, Q_2.realy1, Q_2.realx2, Q_2.realy2, Q_3.realx1,
+      Q_3.realy1, Q_3.realx2, Q_3.realy2)
 
 
