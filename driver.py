e50a968cdc72c81a019ddae77b24455d20d89fdc
Christan <christanskordas@gmail.com>
2017-04-21 19:03:55 +0300

    driver

--- driver.py
+++ driver.py
@@ -3,22 +3,22 @@ from distortion import *
 from auditor_check_overlapping import *
 
 
-K = 87
+K = 2
 userid = 1
 Rmax = 1.7
 Rmin = 1.0
 
 Q = []
 
-Q_1 = Query(1, 500000, 473318.0, 4201118.0, 480000.0, 4209000.0)
+Q_1 = SQuery(1, 500000, 473318.0, 4201118.0, 480000.0, 4209000.0)
 Q_1.episodes = 'CAR'
 Q.append(Q_1)
 
-#Q_2 = Query(2, 500000, 472518.0, 4201118.0, 482000.0, 4206000.0)
-#Q_2.episodes = 'CAR'
-#Q.append(Q_2)
+Q_2 = SQuery(2, 500000, 472518.0, 4201118.0, 482000.0, 4206000.0)
+Q_2.episodes = 'CAR'
+Q.append(Q_2)
 
-#Q_3 = Query(3, 500000, 460000.0, 4200000.0, 470000.0, 4202000.0)
+#Q_3 = SQuery(3, 500000, 460000.0, 4200000.0, 470000.0, 4202000.0)
 #Q_3.episodes = 'MOVE'
 #Q.append(Q_3)
 
@@ -29,22 +29,35 @@ distortionData = DistortionData(2500.0, 0, True)
 # set queries for distortion data
 distortionData.set_subqueries(Q)
 
-ntr = len(Query.combine(Q))
+ntr = len(SQuery.combine(Q))
+print(ntr)
+
 if ntr < K and ntr != 0:
+    if len(Q) == 1:
+        raise ValueError("zoom out can not work with only one subquery. Refuse answer.")
+
     FQ = zoom(K, Q, Rmin, Rmax, distortionData)
     print(Q_1.realx1, Q_1.realy1, Q_1.realx2, Q_1.realy2)
-    if check_for_overlap(K, FQ, userid) is True:
+    querytoadd = []
+    for i in range(len(FQ)):
+        sq = FQ[i]
+        querytoadd.append(SQuery.conversion(sq))
+    if check_for_overlap(K, querytoadd, userid, FQ) is True:
         print("We have overlap the user must not get an answer")
     else:
         print("The user can safely get the answer to his query")
-        result = db.QueriesCollection.insert_one({"urid": userid, "query": FQ})
+        result = db.QueriesCollection.insert_one({"urid": userid, "query": querytoadd})
 elif ntr >= K and ntr != 0:
     FQ = Q
-    if check_for_overlap(K, FQ, userid) is True:
+    querytoadd = []
+    for i in range(len(FQ)):
+        sq = FQ[i]
+        querytoadd.append(SQuery.conversion(sq))
+    if check_for_overlap(K, querytoadd, userid, FQ) is True:
         print("We have overlap the user must not get an answer")
     else:
         print("The user can safely get the answer to his query")
-        result = db.QueriesCollection.insert_one({"urid": userid, "query": FQ})
+        result = db.QueriesCollection.insert_one({"urid": userid, "query": querytoadd})
 else:
     print("The subqueries have no common trajectories")
 
